397d094c3c416eb35ce05684bea6780ab60e10e0
Manuh08 <manuelasoarespacheco11@gmail.com>
2024-11-27 11:18:08 -0400

    Atividade barra de pesquisa
    27-11

--- src/Listarprodutos.jsx
+++ src/Listarprodutos.jsx
@@ -13,7 +13,7 @@ export default function Listarprodutos({produtos}) {
                     <h2>{produto.title}</h2>
                     <p>{produto.description}</p>
                     <p>{produto.category}</p>
-                    <p>{produto.price}</p>
+                    <p>R${produto.price}</p>
                     <img src={produto.image} alt={produto.title} width={100}/>
                 </li>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,84 @@
 import { useEffect, useState } from "react";
-import Listarprodutos from '../Listarprodutos'
-
-
+import Listarprodutos from '../Listarprodutos';
 
 export default function Home() {
 
-    const [ lista, setLista] = useState([]);
-    
+    const [lista, setLista] = useState([]);
+    const [listaCompleta, setListaCompleta] = useState([]);
+
     useEffect(() => {
         const receberListaProdutos = async () =>  {
             try {
-            const resposta = await fetch('https://fakestoreapi.com/products');
-            const dados = await resposta.json();
-            setLista(dados);
+                const resposta = await fetch('https://fakestoreapi.com/products');
+                const dados = await resposta.json();
+                setLista(dados);
+                setListaCompleta(dados);
             } catch (erro) {
-                alert("A conexão com o a API falhou!!!");
+                alert("A conexão com a API falhou!!!");
             }
         }
         receberListaProdutos();
     }, []);
-    if (lista.length === 0) {
-        return <h1>Carregando...</h1>
+
+    if (listaCompleta.length === 0) {
+        return <h1>Carregando...</h1>;
     }
-    const orderAz = () =>{
-        const listaAux = [...lista].sort((a, b)=> a.title.localeCompare(b.title));
+
+    // Função que ordena de A ate Z
+    const orderAz = () => {
+        const listaAux = [...lista].sort((a, b) => a.title.localeCompare(b.title));
         setLista(listaAux);
     }
 
-    const orderZa = () =>{
-        let listaAux = [...lista].sort((a, b)=> a.title.localeCompare(b.title));
-        listaAux = listaAux.reverse()
+      // Função que ordena de Z ate A
+    const orderZa = () => {
+        const listaAux = [...lista].sort((a, b) => a.title.localeCompare(b.title)).reverse();
         setLista(listaAux);
     }
-   
 
+    // Função que ordena do maior preço para o menor preço
+    const MaiorPreco = () => {
+        const listaAux = [...lista].sort((a, b) => b.price - a.price); // Descendente (Maior para o Menor)
+        setLista(listaAux);
+    }
+
+     // Função que ordena do menor preço para o maior preço
+    const MenorPreco = () => {
+        const listaAux = [...lista].sort((a, b) => a.price - b.price); // Ascendente (Menor para o Maior)
+        setLista(listaAux);
+    }
+
+    const pesquisarItem = (valor) =>{
+        if (valor === "") {
+            setLista(listaCompleta);
+            return;
+        }
+
+        const listaAux = lista.filter((item)=>
+            item.title.toLowerCase().includes(valor.toLowerCase())
+
+        );
+        setLista(listaAux);
+    }
 
     return (
         <>
-        
-<button onClick={()=> orderAz()}>Az</button>
-<button onClick={()=> orderZa()}>Za</button>
-        <Listarprodutos produtos={lista}/>
+         <div className="botao">
+            <button className="botao" onClick={orderAz}>Az</button>
+        </div>
+        <div className="botao">
+            <button onClick={orderZa}>Za</button>
+        </div>
+        <div className="botao">
+            <button onClick={MaiorPreco}>Maior</button>
+        </div>
+        <div className="botao">
+            <button onClick={MenorPreco}>Menor</button>
+        </div>
 
+            <input className="pesquisas" placeholder="Pesquisar" onChange={(item) => pesquisarItem(item.target.value)}/>
+
+            <Listarprodutos produtos={lista} />
         </>
     );
 }
